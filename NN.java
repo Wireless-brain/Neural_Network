4f2c34dd23e85078412808fe66d1fcec7dbcb8f9
AVB <antonybyju4444@email.com>
2025-01-27 18:28:25 +0530

    Added child and operation values to the class

--- NN.java
+++ NN.java
@@ -11,13 +11,21 @@ public class NN {
         Value x1w1 = x1.mul(w1); x1w1.label = "x1w1";
         Value x2w2 = x2.mul(w2); x2w2.label = "x2w2";
 
+        // System.out.println("Child and operation of X1: Label " + x1.op);
+
+        // System.out.println("Child and operation of X1W1: " + x1w1.prev[0].label + " " + x1w1.prev[1].label + " " + x1w1.op);
         //System.out.println("Value of x1w1 and x2w2: " + x1w1 + " " + x2w2);
+        // System.out.println("Child and operation of X2W2: " + x2w2.prev[0].label + " " + x2w2.prev[1].label + " " + x2w2.op);
 
         Value x1w1x2w2 = x1w1.add(x2w2); x1w1x2w2.label = "x1w1x2w2";
+        // System.out.println("Child and operation of X1W1x2w2: " + x1w1x2w2.prev[0].label + " " + x1w1x2w2.prev[1].label + " " + x1w1x2w2.op);
+
         Value n = x1w1x2w2.add(b); n.label = "n";
+        // System.out.println("Child and operation of n: " + n.prev[0].label + " " + n.prev[1].label + " " + n.op);
         
         //System.out.println("Before tanh(): " + n);
         Value out = n.tanh(); out.label = "tanh";
+        // System.out.println("Child and operation of out: " + out.prev[0].label + " " + out.op);
 
         System.out.println(out);
     }
--- NN/Value.java
+++ NN/Value.java
@@ -5,14 +5,22 @@ public class Value{
     double data;
     double grad;
     public String label;
-    
+    public String op;
+    public Value[] prev = new Value[2];
+    int i;
+
     public Value (double data, String label){
         this.data = data;
         this.label = label;
+        this.op = null;
+        this.i = -1;
+        this.grad = 0;
     }
 
     public Value(double data){
         this.data = data;
+        this.grad = 0;
+        this.i = -1;
     }
 
     @Override
@@ -22,22 +30,40 @@ public class Value{
 
     public Value add(Value a){
         Value out = new Value(this.data + a.data);
+        out.prev[++i] = a;
+        out.prev[++i] = this;
+        out.op = "+";
+        // public void backward()
+        // this.grad = 1.0 * out.grad;
+        // a.grad = 1.0 * out.grad;
         return out;
     }
+
     public Value sub(Value a){
         Value out = new Value(this.data - a.data);
+        out.prev[++i] = a;
+        out.prev[++i] = this;
+        out.op = "-";
         return out;
     }
+
     public Value mul(Value a){
         Value out = new Value(this.data * a.data);
+        out.prev[++i] = a;
+        out.prev[++i] = this;
+        out.op = "*";        
         return out;
     }
+
     public Value div(Value a){
         if (a.data == 0){
             System.out.println("Error: Division by zero is not possible.");
             return null;
         }
         Value out = new Value(this.data / a.data);
+        out.prev[++i] = a;
+        out.prev[++i] = this;
+        out.op = "/";
         return out;
     }
     
@@ -45,8 +71,8 @@ public class Value{
         double a = this.data;
         double res = (Math.exp(a*2)-1)/(Math.exp(a*2)+1);
         Value out = new Value(res);
+        out.prev[++i] = this;
+        out.op = "tanh";
         return out;
     }
-
-
 }
